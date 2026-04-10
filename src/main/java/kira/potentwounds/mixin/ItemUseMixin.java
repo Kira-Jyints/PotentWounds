d8ceae660d5b031f329463cd92f7d59f53fdde3d
Kira-Jyints <marston2118@gmail.com>
2026-04-10 07:53:29 -0400

    Updated to Minecraft 26.1.2, updated various dependencies.

--- src/main/java/kira/potentwounds/mixin/ItemUseMixin.java
+++ src/main/java/kira/potentwounds/mixin/ItemUseMixin.java
@@ -1,13 +1,12 @@
 package kira.potentwounds.mixin;
 
 import kira.potentwounds.WoundFoodHandler;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
